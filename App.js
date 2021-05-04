e768d4aadb9950a9f45dd8833ab8e11b643e884f
andrywlima <andrywmarcelo.lima@gmail.com>
2021-05-04 13:03:10 -0300

    Chore: Commit inicial

---
+++ App.js
@@ -0,0 +1,38 @@
+
+import React, { Component } from 'react';
+import type {Node} from 'react';
+import {Text,View, Style} from 'react-native';
+
+
+export default class App1 extends Component {
+  render(){
+    return(
+      <View>
+        <Text>
+          Testando essa View
+        </Text>
+      </View>
+    );
+  }
+}
+
+/*
+export default function App1(){
+  return (
+    <View>
+   <View>
+     <Text>CFB Cursos</Text>
+     <Text>Curso React native</Text>
+   </View>
+   <View>
+   <Text>CFB Cursos</Text>
+   <Text>Curso React native</Text>
+ </View>
+ <View>
+ <Text>CFB Cursos</Text>
+ <Text>Curso React native</Text>
+</View>
+</View>
+  );
+};
+*/
